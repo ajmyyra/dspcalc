a44d6d9ae4c74935a7b815c90aeb4997fa5dd567
Antti Myyrä <antti.myyra@sigmatic.fi>
2017-02-20 03:04:05 +0200

    Step 2, variation 1 working. \o/ Initialised steps 2 and 3.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dspcalc - Distributed calculator for UH course Distributed Systems Project
 
 Different steps in different folders.
+
--- step2/server.js
+++ step2/server.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var url = require('url');
-var plotter = require('plotter');
+var gnuplot = require('gnuplot');
+var fs = require('fs');
+var path = require('path');
 const serverport = 8081;
 const origin = 'http://localhost:8000';
+const tempPlotFile = 'plot.temp.dat';
+const sinPlotFile = 'sinplot.png';
 
 function handleRequest(request, response) {
 	response.setHeader('Access-Control-Allow-Origin', origin);
@@ -10,11 +14,35 @@ function handleRequest(request, response) {
 	if (request.method === 'GET') {
 		var params = url.parse(request.url, true);
 
-		if (params.query.op === 'sin') {
-			console.log(params); //debug
-			response.statusCode = 200;
-            response.setHeader('Content-Type', 'application/json');
-            response.end(JSON.stringify( { 'result': 'yees yees' } ));
+		if (params.query.op === 'sinselfcreated') {
+			
+            var filePath = path.join(__dirname, sinPlotFile);
+            
+            handleSinFormData(params.query, function(result) {
+            	createSinPlot(result, function(plotfile) {
+            		console.log(new Date() + ' Temporary plot image file ' + plotfile + ' created succesfully.');
+            		
+            		// Timeout is needed as gnuplot is executed in async manner, ending when file isn't
+            		// yet saved to disk. We'll wait for 100 milliseconds for it. Other possibility is
+            		// to set up fs.watch(), but it's still unstable and fires of 2-N times.
+            		setTimeout(function(){
+            			fs.readFile(filePath, function(err, data) {
+    						response.writeHead(200, {
+    							'Content-Type': 'image/png'
+    						});
+    						response.end(new Buffer(data).toString('base64'), function() {
+    							fs.unlink(tempPlotFile, function() {
+    								console.log(new Date() + ' Temporary datafile ' + tempPlotFile + ' for gnuplot deleted.');
+    							});
+    							fs.unlink(sinPlotFile, function() {
+    								console.log(new Date() + ' Temporary plot image file ' + sinPlotFile + ' deleted.');
+    							});
+    						});
+    					});
+    				}, 100);
+            	});
+            });
+            return;
 		}
 
         if (isNumber(params.query.arg1) && isAllowedMethod(params.query.op) && isNumber(params.query.arg2)) {
@@ -69,6 +97,52 @@ function isAllowedMethod(m) {
 function badRequest(response) {
 	response.statusCode = 400;
 	response.end();
+}
+
+function handleSinFormData(params, callback) {
+	var regexp = /\[([^\]]+)\]/;
+	var plotpoints = {};
+
+	for (var property in params) {
+		if (property.match(/^plot\[/)) {
+        	var key = regexp.exec(property)[1];
+        	plotpoints[key] = params[property];
+        }
+    }
+
+    if (callback) callback(plotpoints);
+}
+
+function createSinPlot(plotpoints, callback) {
+	var stream = fs.createWriteStream(tempPlotFile);
+	stream.once('open', function(fd) {
+		stream.write("#Temporary data file for n*sin(x) data points\n");
+
+		for (x in plotpoints) {
+			stream.write(x + "\t" + plotpoints[x] + "\n");
+		}
+
+		stream.on('close', function() {
+			console.log(new Date() + ' Temporary datafile for gnuplot created succesfully.');
+			var plot = gnuplot()
+				.set('term png')
+				.set('output "' + sinPlotFile + '"')
+				.set('notitle')
+				.set('nokey')
+				.plot('"' + tempPlotFile + '" with lines')
+				.end();
+
+			callback(sinPlotFile); 
+		});
+
+
+		stream.end();
+	});
+}
+
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
 }
 
 var server = http.createServer(handleRequest);
